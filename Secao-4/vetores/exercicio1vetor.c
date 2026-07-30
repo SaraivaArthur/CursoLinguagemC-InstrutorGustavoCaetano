19f32ab2d6e4872614a996f20eb1ded319f384b1
Arthur <arthurdfranca09@gmail.com>
2026-07-30 12:06:47 -0300

    Atividade 1 Vetores

---
+++ Secao-4/vetores/exercicio1vetor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    float fps[6], soma = 0, media;
+    
+    printf("==== Digite o FPS registrados em 6 momentos: ====\n");
+    
+    for(int i = 0; i < 6; i++) {
+        printf("Digite o FPS %d: ", i + 1);
+        scanf("%f", &fps[i]);
+        soma += fps[i];
+    }
+    
+    media = soma / 6;
+    
+    printf("\nMedia de FPS: %.2f", media);
+
+    return 0;
+}
